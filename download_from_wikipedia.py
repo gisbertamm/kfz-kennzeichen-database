a2d719af286754639f869a4d06bfe13370f9fd37
Gisbert Amm <gisbert.amm@web.de>
2015-07-08 12:28:00 +0200

    first draft of download script

---
+++ download_from_wikipedia.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from bs4 import BeautifulSoup
+import urllib2
+from proxy_url import proxy_url
+
+print "Configuring Proxy..."
+proxy = urllib2.ProxyHandler({'https': proxy_url})
+auth = urllib2.HTTPBasicAuthHandler()
+opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
+urllib2.install_opener(opener)
+
+response = urllib2.urlopen('https://de.wikipedia.org/wiki/Liste_der_Kfz-Kennzeichen_in_Deutschland')
+print "Downloading Kfz-Kennzeichen in Deutschland..."
+html = response.read()
+
+print "Parsing Kfz-Kennzeichen in Deutschland..."
+soup = BeautifulSoup(html, 'html.parser')
+result = []
+
+for table in soup.find_all('table'):
+    if table.tr.th.get_text() == 'Abk.':
+        for row in table.find_all('tr'):
+            row_result = []
+            for cell in row.find_all('td'):
+                row_result.append(cell.get_text())
+            if len(row_result) > 0:
+                result.append(row_result)
+
+for row in result:
+    print row
