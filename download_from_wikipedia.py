ba5e1d127cdc37edc687347491b9d614e504c608
Gisbert Amm <gisbert.amm@web.de>
2015-07-08 14:23:47 +0200

    correctly handle entries with rowspan=3

--- download_from_wikipedia.py
+++ download_from_wikipedia.py
@@ -17,33 +17,38 @@ html = response.read()
 print "Parsing Kfz-Kennzeichen in Deutschland..."
 soup = BeautifulSoup(html, 'html.parser')
 result = []
-row_should_be_cloned = False
+row_should_be_cloned = 0
 
 for table in soup.find_all('table'):
     if table.tr.th.get_text() == 'Abk.':
         for row in table.find_all('tr'):
-            if row_should_be_cloned:
-                row_should_be_cloned = False
-                row_result = result[-1]
-                td_list = row.find_all('td')
-                for cell in td_list:
-                    # overwrite existing values - only the second cell differs
-                    if cell.get_text():
-                        row_result[1] = cell.get_text()
+            if row_should_be_cloned > 0:
+                for i in range(row_should_be_cloned):
+                    # create clone
+                    row_result = result[-1]
+                    clone = []
+                    for elem in row_result:
+                        clone.append(elem)
+                    result.append(clone)
+
+                    # change clone
+                    for cell in row.find_all('td'):
+                        # overwrite existing values - only the second cell differs
+                        if cell.get_text():
+                            clone[1] = cell.get_text()
+                row_should_be_cloned -= 1
             else:
                 row_result = []
                 for cell in row.find_all('td'):
                     if cell.get('rowspan') == '2' and cell.parent.name == 'tr':
                         # clone row later
-                        row_should_be_cloned = True
+                        row_should_be_cloned = 1
+                    if cell.get('rowspan') == '3' and cell.parent.name == 'tr':
+                        # clone row later
+                        row_should_be_cloned = 2
                     row_result.append(cell.get_text())
                 if len(row_result) > 0:
                     result.append(row_result)
-                    if row_should_be_cloned:
-                        clone = []
-                        for elem in row_result:
-                            clone.append(elem)
-                        result.append(clone)
 
 for row in result:
     print row
