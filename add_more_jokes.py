1d25f94fec7d9e79337f44e89ed11eea39d66b79
Gisbert Amm <gisbert.amm@web.de>
2015-08-25 11:50:06 +0200

    added some more jokes

--- add_more_jokes.py
+++ add_more_jokes.py
@@ -12,18 +12,36 @@ con = sqlite3.connect("NumberplateCodesManager.sqlite")
 with open('more_jokes.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=';', quotechar='|')
     for row in csvreader:
-        print "Code: " + row[0]
+        print
+        code = row[0].strip()
+        joke = row[1].strip()
+        print "Code: " + code
+        print "Joke:     " + joke
 
         cur = con.cursor()    
-        cur.execute("SELECT jokes FROM jokes WHERE code = '" + row[0] + "'")
+        cur.execute("SELECT jokes FROM jokes WHERE code = '" + code + "'")
     
         db_rows = cur.fetchall()
+
+        joke_already_in_db = False
  
         for db_row in db_rows:
-            print "Checking against DB: " + db_row[0] 
-            if db_row[0] == row[1]:
-                print "Joke already in database " + row[1]
-            else:
-                print "Adding joke to database " + row[1]
+            print "db entry: " + db_row[0] 
+            if db_row[0] == joke:
+                joke_already_in_db = True
+
+        if joke_already_in_db:
+            pass
+            print "Joke already in database: " + joke
+        else:
+            print "Adding joke to database: " + joke
+            cur.execute("SELECT MAX(_id)jokes FROM jokes")
+            id_row = cur.fetchone()
+            id = str(id_row[0] + 1)
+            statement = "INSERT INTO jokes VALUES (" + id + ", " + "'" + code + "', '" + joke + "')"
+            print statement
+            cur.execute(statement)
+            con.commit()
 
 con.close()
+
